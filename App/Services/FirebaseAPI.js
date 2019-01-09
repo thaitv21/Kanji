82effe68319b3327cb429b414a5a64b65d2eef3f
toanqn <toanquach.bk59@gmail.com>
2019-01-10 00:40:07 +0700

    add firebase key

--- App/Services/FirebaseAPI.js
+++ App/Services/FirebaseAPI.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyABVV2BneQyXmTvl9lr6w6kzk2Fraex2YE",
-    authDomain: "kanji-b875f.firebaseapp.com",
-    databaseURL: "https://kanji-b875f.firebaseio.com",
-    projectId: "kanji-b875f",
-    storageBucket: "kanji-b875f.appspot.com",
-    messagingSenderId: "936742228449"
+    apiKey: "AIzaSyBvCvUbzv8H88ED-yJFfN0985qA0heE2YI",
+    authDomain: "kanji-f447b.firebaseapp.com",
+    databaseURL: "https://kanji-f447b.firebaseio.com",
+    projectId: "kanji-f447b",
+    storageBucket: "kanji-f447b.appspot.com",
+    messagingSenderId: "796438937263"
 };
 
 firebase.initializeApp(config);
